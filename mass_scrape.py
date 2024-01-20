fe6ac77c3826d02ee211c9fe2e4976be35cbf9d3
hannahz0 <85980913+hannahz0@users.noreply.github.com>
2024-01-19 18:06:20 -0800

    Chrome webdriver

--- mass_scrape.py
+++ mass_scrape.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 import time                                                                                                                                                                                                                                                            
 
-driver = webdriver.Chrome(executable_path='/Users/hannahzhang/Downloads/chromedriver_mac_arm64/chromedriver.exe')
+driver = webdriver.Chrome()
 driver.get('https://www.lttstore.com/products/bits')
 driver.maximize_window()
 time.sleep(1)
+
+cookie = driver.find_element_by_xpath('//button[@id="Cookies-button"]')
+
+try:
+    cookie.click()
+except:
+    pass
 
