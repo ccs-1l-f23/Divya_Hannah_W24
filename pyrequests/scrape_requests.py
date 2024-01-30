4af309456df37f271bb0f61eb40ab096aab93c01
hannahz0 <85980913+hannahz0@users.noreply.github.com>
2024-01-29 21:41:54 -0800

    final csv file

--- pyrequests/scrape_requests.py
+++ pyrequests/scrape_requests.py
@@ -11,28 +11,37 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import html
+from urllib.parse import unquote
+import lxml.html
+
 
 results = []
 page_number = 1
 
 while True:
+
     response = requests.get(f"https://judge.me/reviews/reviews_for_widget?url=linus-tech-tips-store.myshopify.com&shop_domain=linus-tech-tips-store.myshopify.com&platform=shopify&page={page_number}&per_page=5&product_id=6692255891559")
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-    # text = soup.find_all("class":"jdgm-rev__body")
-    results.append(soup)
+    # soup = BeautifulSoup(response.text, 'html.parser')
+
+    soup = lxml.html.fromstring(response.text).text_content()
+
+    print(soup)
+
+    # results.append(soup)
 
     if page_number == 61:
         break
 
     page_number = page_number + 1
 
-print(results)
+# print(results)
 
-with open('reviews.csv', 'a') as csv_file:
- writer = csv.writer(csv_file)
- for result in results:
-    writer.writerow(result)
+# with open('reviews.csv', 'a') as csv_file:
+#  writer = csv.writer(csv_file)
+#  for result in results:
+#     writer.writerow(result)
 
 
 
