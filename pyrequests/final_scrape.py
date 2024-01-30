8b524caa6bc1ef7c45e7b71883689e048a431ed7
divya-subramonian25 <divya.subramonian25@gmail.com>
2024-01-29 22:08:26 -0800

    Scraped screwdriver reviews and put info in CSV

---
+++ pyrequests/final_scrape.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+import csv
+
+f = csv.writer(open('screwdriver.csv', 'w'))
+
+with open('reviews_final.csv') as html_obj:
+    for row in html_obj: 
+        soup = BeautifulSoup(row, 'html.parser')
+
+        f.writerow(['Numerical Ratings:'])
+
+        for numerical_rating in soup.find_all():
+            if "data-score" in numerical_rating.attrs:
+                content = numerical_rating["data-score"]
+                print(content)
+                f.writerow([content])
+
+        print("**************")
+        f.writerow(['Helpful:'])
+
+        for helpful in soup.find_all():
+            if "data-thumb-up-count" in helpful.attrs:
+                content = helpful["data-thumb-up-count"]
+                print(content)
+                f.writerow([content])
+
+        print("**************")
+        f.writerow(['Not helpful:'])
+
+        for not_helpful in soup.find_all():
+            if "data-thumb-down-count" in not_helpful.attrs:
+                content = not_helpful["data-thumb-up-count"]
+                print(content)
+                f.writerow([content])
+            
+        print("**************")
+        f.writerow(['Written Reviews:'])
+
+        for review_data in soup.find_all('div', attrs = {"class":"jdgm-rev__content"}):
+            if (review_data.find("p")):
+                content = review_data.find("p").text
+                print(content)
+                print("-----")
+                f.writerow([content])
